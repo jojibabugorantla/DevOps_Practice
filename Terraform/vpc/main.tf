a4c926e7a702703dc4c644021acdf6622a37af04
Joji Babu Gorantla <jojibabugorantla@gmail.com>
2024-06-20 23:11:40 +0530

    added subnet and it is also a implicit dependency

--- Terraform/vpc/main.tf
+++ Terraform/vpc/main.tf
@@ -7,3 +7,30 @@ resource "aws_vpc" "vpc" {
     }
   
 }
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "192.168.1.0/24"
+
+  tags       = {
+    Name     = "subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "192.168.2.0/24"
+
+  tags       = {
+    Name     = "subnet2"
+  }
+}
+
+resource "aws_subnet" "subnet3" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "192.168.3.0/24"
+
+  tags       = {
+    Name     = "subnet3"
+  }
+}
--- Terraform/vpc/variable.tf
+++ Terraform/vpc/variable.tf
@@ -1,9 +1,11 @@
 variable "aws-region" {
     type = string
     description = "Enter the region name like us-east-1"
+    default = "us-west-1"
 }
 variable "cidr_block" {
     type = string
     description = "Enter the cidr block range like 10.0.0.0/16"
+    default = "192.168.0.0/16"
   
 }

5a7ef6769492e1905b54a5277ca70b45e1dbac57
Joji Babu Gorantla <jojibabugorantla@gmail.com>
2024-06-20 22:37:51 +0530

    added variables file

--- Terraform/vpc/provider.tf
+++ Terraform/vpc/provider.tf
@@ -1,14 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 }
-
 provider "aws" {
-  region = "us-west-1"
+  region      = var.aws-region
   # Configure the below details in the aws configure console to
   # access_key = ""
   # secret_key = ""
---
+++ Terraform/vpc/variable.tf
@@ -0,0 +1,9 @@
+variable "aws-region" {
+    type = string
+    description = "Enter the region name like us-east-1"
+}
+variable "cidr_block" {
+    type = string
+    description = "Enter the cidr block range like 10.0.0.0/16"
+  
+}

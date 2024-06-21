23caf7145a95b8f07fe19e562479875dfefc87b2
Joji Babu Gorantla <jojibabugorantla@gmail.com>
2024-06-21 12:10:07 +0530

    used remote module in the repo

--- Terraform/module_usage_vpc/main.tf
+++ Terraform/module_usage_vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source       = "./module/vpc"
+  source       = "git::https://github.com/jojibab/terraform_module"
   aws_region   = "us-west-2"
   cidr_block   = "192.168.0.0/16"
   subnet_names = ["app", "web", "database"]
